6aac638b260b013c4c73d0c81ba55493c3b4c4de
hhh@AIMC-714-hhh-wsl-ubuntu22 <1340545944@qq.com>
2025-07-28 15:11:54 +0800

    🛠️ Fix docs in `dmlx.property`

--- src/dmlx/property.py
+++ src/dmlx/property.py
@@ -9,9 +9,6 @@ def param(cls: type[click.Parameter], *args, **kwargs) -> property:
     """Create a param property that is to be read from the experiment command.
     (This is a helper function that invokes `experiment.param()` on the
     active experiment in current context.)
-
-    Returns:
-        param (click.Parameter): The created param.
     """
     return get_current_experiment().param(cls, *args, **kwargs)
 
@@ -20,9 +17,6 @@ def argument(*args, **kwargs) -> property:
     """Create an argument property that is to be read from the experiment command.
     (This is a helper function that invokes `experiment.argument()` on the
     active experiment in current context.)
-
-    Returns:
-        argument (click.Argument): The created argument.
     """
     return get_current_experiment().argument(*args, **kwargs)
 
@@ -31,9 +25,6 @@ def option(*args, **kwargs) -> property:
     """Create an option property that is to be read from the experiment command.
     (This is a helper function that invokes `experiment.option()` on the
     active experiment in current context.)
-
-    Returns:
-        option (click.Option): The created option.
     """
     return get_current_experiment().option(*args, **kwargs)
 
